fdddc470081a899b8c0e978b2e26ab1f00e8b407
Viktória Madarász <mustela.erminea.parva@gmail.com>
2018-06-29 11:55:08 +0200

    Products data is broaded,Additional tasks added

---
+++ Additional tasks/products-array.js
@@ -0,0 +1,252 @@
+var products = [
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }   
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }    
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+    {
+        id: ;
+        name: "";
+        cathegory: ;
+        price: ;
+        count: ;
+        sold: ;
+        createdAt: ;
+        updatedAt: ;
+    }
+]
